0461be8aab2a8320e1af810b243f5d6a0afc8370
Paul Sava <andreisava1337@gmail.com>
2023-10-22 20:23:37 +0200

    fixed names

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className='mx-auto w-full max-w-prose pb-6 pl-6 pr-6 text-center text-gray-400'>
       <div className='flex flex-col items-center text sm text-gray-400'>
         <a className='mx-auto'>
-          Made with <HeartIcon className='mx-1 fill-gray-400' /> by the
+          Made with <HeartIcon className='w-fill fill-gray-400' /> by the
           PularMoney team
         </a>
       </div>
